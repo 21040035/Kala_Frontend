737bbaa75c5465740ab8b3731409eeca3ed1d15d
Smriti Ghale <np01cp4a210069@islingtoncollege.edu.np>
2024-04-14 07:56:23 +0545

    nav bar added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     <div className="min-h-screen bg-gray-300 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Login to your account navbar
+          Login to your account 
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
